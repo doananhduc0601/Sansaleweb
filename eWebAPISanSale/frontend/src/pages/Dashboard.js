6e4fc0201923c64e8ce826c7307692c96f5e7bd9
tan vu <vtan01.04@gmail.com>
2021-10-11 09:59:32 +0700

    sửa cây thư mục

---
+++ eWebAPISanSale/frontend/src/pages/Dashboard.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { getToken, getUser, removeUserSession } from "../Utils/Common";
+import Nav from "../components/Nav";
+import axios from "axios";
+
+function Dashboard(props) {
+  const name = getUser();
+  const token = getToken();
+
+  const handleLogout = () => {
+    removeUserSession();
+    props.history.push("/login");
+  };
+  axios
+    .get("https://localhost:5001/api/Categories", {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+    .then(
+      (response) => {
+        //var response = response.data;
+      },
+      (error) => {
+        removeUserSession();
+        props.history.push("/login");
+      }
+    );
+
+  // const token = getToken();
+  // const name = getUser();
+  // // handle click event of logout button
+  // const handleLogout = () => {
+  //   removeUserSession();
+  //   props.history.push('/login');
+  // }
+
+  // if (token==null) {
+  //   return <div className="content">Checking Authentication...</div>
+  // }
+
+  return (
+    <div>
+      <Nav />
+      <input type="button" onClick={handleLogout} value="Logout" />
+      <br />
+      <br />
+      <h1> Welcome {name}!</h1>
+    </div>
+  );
+}
+export default Dashboard;
