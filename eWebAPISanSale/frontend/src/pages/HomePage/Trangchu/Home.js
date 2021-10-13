4266562c39fde5065babc3cdf98f0c38b74d8a55
Đoàn Anh Đức <ducpda111@gmail.com>
2021-10-13 21:49:14 +0700

    A

--- eWebAPISanSale/frontend/src/pages/HomePage/Trangchu/Home.js
+++ eWebAPISanSale/frontend/src/pages/HomePage/Trangchu/Home.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react';
 import './Home.css';
 import Slidecare from '../../../components/slidecare/slidecare';
 import Slides from '../../../components/slide/slide';
+import slide1 from '../../../wwwroot/Image/slide1.jpg'
+import Menuhome from '../../../components/menuhome/menuhome';
+import Baiviet from '../BaiViet/Baiviet';
 
 class Home extends Component {
     render() {
         return (
             <div>
-      
+                 <Menuhome/>
+            <img src={slide1}/>
+            <Baiviet/>
             <Slides/>
            <Slidecare/>
             </div>
