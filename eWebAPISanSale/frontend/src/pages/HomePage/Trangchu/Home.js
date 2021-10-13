3270eb90947cb4eca34e4a1f7a93aacf56db1c1a
Đoàn Anh Đức <ducpda111@gmail.com>
2021-10-13 22:05:12 +0700

    ấdasd

--- eWebAPISanSale/frontend/src/pages/HomePage/Trangchu/Home.js
+++ eWebAPISanSale/frontend/src/pages/HomePage/Trangchu/Home.js
@@ -1,23 +1,23 @@
-import React, { Component } from 'react';
-import './Home.css';
-import Slidecare from '../../../components/slidecare/slidecare';
-import Slides from '../../../components/slide/slide';
-import slide1 from '../../../wwwroot/Image/slide1.jpg'
-import Menuhome from '../../../components/menuhome/menuhome';
-import Baiviet from '../BaiViet/Baiviet';
+// import React, { Component } from 'react';
+// import './Home.css';
+// import Slidecare from '../../../components/slidecare/slidecare';
+// import Slides from '../../../components/slide/slide';
+// import slide1 from '../../../wwwroot/Image/slide1.jpg'
+// import Menuhome from '../../../components/menuhome/menuhome';
+// import Baiviet from '../BaiViet/Baiviet';
 
-class Home extends Component {
-    render() {
-        return (
-            <div>
-                 <Menuhome/>
-            <img src={slide1}/>
-            <Baiviet/>
-            <Slides/>
-           <Slidecare/>
-            </div>
-        );
-    }
-};
+// class Home extends Component {
+//     render() {
+//         return (
+//             <div>
+//                  <Menuhome/>
+//             <img src={slide1}/>
+//             <Baiviet/>
+//             <Slides/>
+//            <Slidecare/>
+//             </div>
+//         );
+//     }
+// };
 
-export default Home;
+// export default Home;
