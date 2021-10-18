ea21399702345281f4e9287c76728299a93e2bf5
Đoàn Anh Đức <ducpda111@gmail.com>
2021-10-19 00:01:32 +0700

    add product thêm sửa xoá

--- eWebAPISanSale/frontend/src/components/ProductList/component_product/TaskForm.js
+++ eWebAPISanSale/frontend/src/components/ProductList/component_product/TaskForm.js
@@ -1,45 +1,42 @@
-import React, { useState, useEffect } from "react";
-import { AiFillCloseCircle } from "react-icons/ai";
+import React, { useState, useEffect } from 'react'
+import {RiAddCircleLine, } from "react-icons/ri";
+import {AiOutlineCloseCircle } from "react-icons/ai";
+import '../Product.css'
 
-const defaultImageSrc = "/img/thecao.png";
+const defaultImageSrc = "../assets/img/damir-bosnjak.jpg";
 
 const initialFieldValues = {
   id: 0,
-  name: "",
+  name: "Tên Sản Phẩm",
   //code: "",
   metaTitle: "",
   description: "",
   image: "",
   //moreImages: "",
   price: "",
-  //promotionPrice: "",
+  promotionPrice: "",
   //includedVat: "",
   quantity: "",
   categoryId: "",
   //detail: "",
   warranty: "",
   createdDate: "",
-  createdBy: "",
-  //modifiedDate: "",
+  //createdBy: "",
+  modifiedDate: "",
   //modifiedBy: "",
   metaKeywords: "",
   metaDescriptions: "",
   status: "",
   //topHot: "",
-  //viewCount: "",
+  viewCount: "",
   link: "",
   // category: null,
   imageSrc: defaultImageSrc,
   imageFile: null,
 };
+export default function TaskFrom(props) {
+  const [isVisible, setIsVisible] = useState(false);
 
-export default function TaskForm(props) {
-  ///////////////////// I. thành phần hiển thị bên trái
-  // constructor(props) = () => {
-  //   super(props);
-  //   this.state = { id: "11", name: "22", status: false };
-  // }
-  ////////////////////////////////// Đóng thành phần bên trái //////////////////////////////////
   const onCloseForm = () => {
     this.props.onCloseForm();
   };
@@ -127,11 +124,11 @@ export default function TaskForm(props) {
       formData.append("quantity", values.quantity);
       formData.append("categoryId", values.categoryId);
       formData.append("detail", values.detail);
-      formData.append("warranty", values.warranty);
-      formData.append("createdDate", values.createdDate);
-      formData.append("createdBy", values.createdBy);
-      formData.append("modifiedDate", values.modifiedDate);
-      formData.append("modifiedBy", values.modifiedBy);
+     // formData.append("warranty", values.warranty);
+     // formData.append("createdDate", values.createdDate);
+     // formData.append("createdBy", values.createdBy);
+     // formData.append("modifiedDate", values.modifiedDate);
+     // formData.append("modifiedBy", values.modifiedBy);
       formData.append("metaKeywords", values.metaKeywords);
       formData.append("status", values.status);
       formData.append("viewCount", values.viewCount);
@@ -144,229 +141,194 @@ export default function TaskForm(props) {
 
   const applyErrorClass = (field) =>
     field in errors && errors[field] == false ? " invalid-field" : "";
-
-  ///////////////////
-  return (
-    <>
-    <div class="panel panel-warning">
-      {/* 0. */}
-      <div class="panel-heading">
-        <h3 class="panel-title">
-          Thêm Công Việc------------
-          <AiFillCloseCircle
-            style={{ color: "red", fontSize: "1.7em" }}
-            onClick={onCloseForm}
-            text-align="center"
-          />
-        </h3>
-      </div>
-
-      <div class="panel-body">
-        {/* chuyền submit vào */}
-        <form autoComplete="off" noValidate onSubmit={handleFormSubmit}>
-          {/* 1. Hiển thị Category và chọn */}
-          <div class="form-group">
-            <label>Danh mục :</label>
-            <select class="form-control" required="required">
-              <option value="0">Điện tử</option>
-              <option value="1">Mỹ phẩm</option>
-              <option value="1">Card điện thoại</option>
-              <option value="1">Cần, ke, kẹo, cỏ</option>
-            </select>
-          </div>
-          {/* 2. hình ảnh */}
-          <img
-            src={values.imageSrc}
-            className="card-img-top"
-            style={{ height: 250, width: 320, margin: 4 }}
-          />
-          <div className="form-group">
-            <input
-              type="file"
-              accept="image/*"
-              className={"form-control-file" + applyErrorClass("imageSrc")}
-              onChange={showPreview}
-              id="image-uploader"
-            />
-          </div>
-          {/* 3. Thêm data */}
-          <label>ID :</label>
-          <input
-            class="form-control"
-            name="id"
-            value={values.id}
-            //value={this.state.id}
-            onChange={handleInputChange}
-          />
-          <label>tên sản phẩm:</label>
-          <input
-            type="text"
-            class="form-control"
-            name="name"
-            value={values.name}
-            onChange={handleInputChange}
-          />
-          <label>meta Title:</label>
-          <input
-            type="text"
-            class="form-control"
-            name="metaTitle"
-            value={values.metaTitle}
-            //value={this.state.name}
-            onChange={handleInputChange}
-          />
-          <label>Miêu tả :</label>
-          <input
-            type="text"
-            class="form-control"
-            name="description"
-            value={values.description}
-            onChange={handleInputChange}
-          />
-          {/* <label>Tên image:</label>
-          <input
-            type="text"
-            class="form-control"
-            name="image"
-            value={values.image}
-            onChange={handleInputChange}
-          /> */}
-          <label>Giá đã giảm :</label>
-          <input
-            type="text"
-            class="form-control"
-            name="price"
-            value={values.price}
-            onChange={handleInputChange}
-          />
-          <label>Giá gốc :</label>
-          <input
-            type="text"
-            class="form-control"
-            name="promotionPrice"
-            value={values.promotionPrice}
-            onChange={handleInputChange}
-          />
-          {/* <label>Hàng tồn :</label>
-          <input
-            type="text"
-            class="form-control"
-            name="quantity"
-            value={values.quantity}
-            onChange={handleInputChange}
-          /> */}
-          <label>Category ID :</label>
-          <select
-            class="form-control"
-            required="required"
-            onChange={handleInputChange}
-            name="categoryId"
-            value={values.categoryId}
-          >
-            <option value="1">1. Điện tử</option>
-            <option value="2">2. Mỹ phẩm</option>
-            <option value="3">3. Gia dụng</option>
-            <option value="4">4. Card điện thoại</option>
-          </select>
-
-          {/* <label>Chi tiết :</label>
-          <input
-            type="text"
-            class="form-control"
-            name="detail"
-            value={values.detail}
-            onChange={handleInputChange}
-          />
-          <label>Bảo hiểm :</label>
-          <input
-            type="text"
-            class="form-control"
-            name="warranty"
-            value={values.warranty}
-            onChange={handleInputChange}
-          /> */}
-          <label>Ngày Tạo :</label>
-          <input
-            type="date"
-            class="form-control"
-            name="createdDate"
-            value={values.createdDate}
-            onChange={handleInputChange}
-          />
-          <label>Tạo bởi ai :</label>
-          <input
-            type="text"
-            class="form-control"
-            name="createdBy"
-            value={values.createdBy}
-            onChange={handleInputChange}
-          />
-          {/* <label>ngày sửa :</label>
-          <input
-            type="text"
-            class="form-control"
-            name="modifiedDate"
-            value={values.modifiedDate}
-            onChange={handleInputChange}
-          />
-          <label>sửa lại bởi ai :</label>
-          <input
-            type="text"
-            class="form-control"
-            name="modifiedBy"
-            value={values.modifiedBy}
-            onChange={handleInputChange}
-          />
-          <label>Từ khóa :</label>
-          <input
-            type="text"
-            class="form-control"
-            name="metaKeywords"
-            value={values.metaKeywords}
-            onChange={handleInputChange}
-          /> */}
-          <label>Status:</label>
-          <select
-            class="form-control"
-            required="required"
-            name="status"
-            value={values.status}
-            onChange={handleInputChange}
-          >
-            <option value="True">True</option>
-            <option value="False">False</option>
-          </select>
-
-          {/* <label>Số lượng đã xem :</label>
-          <input
-            type="text"
-            class="form-control"
-            name="viewCount"
-            value={values.viewCount}
-            onChange={handleInputChange}
-          /> */}
-          <label>Link :</label>
-          <input
-            type="text"
-            class="form-control"
-            name="link"
-            value={values.link}
-            onChange={handleInputChange}
-          />
-          <br />
-          <div class="text-center">
-            <button type="submit" class="btn btn-warning">
-              Thêm
+    return (
+        <>
+            <button class="iconmofrom btn-primary"  type="button" onClick={() => setIsVisible(false)} style={ {display : isVisible? "block" :"none"}}>
+            <AiOutlineCloseCircle/>
             </button>
-          </div>
-        </form>
-      </div>
+            <button class="iconmofrom btn-primary" type="button" onClick={() => setIsVisible(true)} style={ {display : isVisible? "none" :"block"}}>
+            <RiAddCircleLine/>
+            </button>
+            <div class="row" style={{ display: isVisible ? "block" : "none" }}>
+        
+          <div class="col-md-4">
+            <div class="card card-user">
+              <div class="image"style={{ height: 50, width: 100,marginTop:6  }}>
+              
+                  {/* style={{ height: 250, width: 320, margin: 4 }} */}
+                
+              </div>
+              
+              
             
-    </div>
-          
-    </>
+              <div class="card-body">
+                <div class="author">
+                 
+                <img src={values.imageSrc} style={{ height: 250, width: 250,marginTop:30 }}/> 
+             
+                    <h5 class="title">{values.name}</h5>
+                 
+                  <p class="description">
+                  {values.metaTitle}
+                  </p>
+                </div>
+                <p class="description text-center">
+                {values.description}
+                </p>
+              </div>
+              <div class="card-footer">
+                
+                <div class="button-container">
+                  <div class="row">
+                   
+                    <div class="col-lg-5 col-md-6 col-6 ml-auto mr-auto">
+                      <h5><del>{values.promotionPrice}VND</del></h5>
+                    </div>
+                    <div class="col-lg-5 mr-auto">
+                      <h5>{values.price}VND</h5>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              </div>
+              <input
+                  type="file"
+                  accept="image/*"
+                  className={"form-control-file" + applyErrorClass("imageSrc")}
+                  onChange={showPreview}
+                  id="image-uploader" />
+              </div>
+            
+             
+          <div class="col-md-8">
+            <div class="card card-user">
+              <div class="card-header">
+                <h5 class="card-title">Edit Product</h5>
+              </div>
+              <div class="card-body">
+                <form  autoComplete="off" noValidate onSubmit={handleFormSubmit}>
+     
+              
+                <div class="row">
+                <div class="col-md-3 pr-1">
+                      <div class="form-group">
+                        <label>Id</label>
+                        <input  class="form-control" placeholder="id" name="id" value={values.id} onChange={handleInputChange}/>
+                      </div>
+                    </div>
+                <div class="col-md-9 pl-1">
+                      <div class="form-group">
+                        <label>Tên Sản Phẩm</label>
+                        <input  class={"form-control" + applyErrorClass("name")} placeholder="Tên sản phẩm" name="name" value={values.name} onChange={handleInputChange}/>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-2 pr-1">
+                      <div class="form-group">
+                        <label>Giá Gốc </label>
+                        <input  class={"form-control" + applyErrorClass("Giá")} disabled="" placeholder="Giá Gốc" name="promotionPrice" value={values.promotionPrice} onChange={handleInputChange}/>
+                      </div>
+                    </div>
+                    <div class="col-md-4 px-1">
+                      <div class="form-group">
+                        <label>Giá Bán</label>
+                        <input type="text" class="form-control" placeholder="Giá Bán" name="price" value={values.price} onChange={handleInputChange}/>
+                      </div>
+                    </div>
+                    <div class="col-md-3 pl-1">
+                      <div class="form-group">
+                        <label for="exampleInputEmail1">Lượng Truy Cập</label>
+                        <input type="text" class="form-control" placeholder=""name="viewCount" value={values.viewCount} onChange={handleInputChange}/>
+                      </div>
+                    </div>
+                    <div class="col-md-3 pl-1">
+                      <div class="form-group">
+                        <label for="exampleInputEmail1">Hàng Tồn</label>
+                        <input type="text" class="form-control" placeholder="Giá bán" name="quantity" value={values.quantity} onChange={handleInputChange}/>
+                      </div>
+                    </div>
+                  </div>
 
-  );
+                  <div class="row">
+                    <div class="col-md-8">
+                      <div class="form-group">
+                        <label>Link Sản Phẩm</label>
+                        <input type="text" class={"form-control" + applyErrorClass("Link sản phẩm")} placeholder="link" name="link" value={values.link} onChange={handleInputChange}/>
+                      </div>
+                    </div>
+                    <div class="col-md-4">
+                      <div class="form-group">
+                        <label>Category</label>
+                        {/* <input type="text" class="form-control" placeholder="category" name="categoryId" value={values.categoryId} onChange={handleInputChange}/> */}
+                        <select type="text" class={"form-control" + applyErrorClass("category")}  name="categoryId" value={values.categoryId} onChange={handleInputChange}>
+                          <option value="1">1 . Thiết Bị Điển Tử</option>
+                          <option value="2">2 . Quẩn Áo </option>
+                          <option value="3">3 . Food</option>
+                          <option value="4">4 . Nội Thất</option>
+                        </select>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-4 pr-1">
+                      <div class="form-group">
+                        <label>Meta </label>
+                        <input type="text" class="form-control" placeholder="meta" name="metaTitle" value={values.metaTitle} onChange={handleInputChange}/>
+                      </div>
+                    </div>
+                    <div class="col-md-4 px-1">
+                      <div class="form-group">
+                        <label>KeyWord</label>
+                        <input type="text" class="form-control" placeholder="keyword" name="metaKeywords" value={values.metaKeywords} onChange={handleInputChange}/>
+                      </div>
+                    </div>
+                    <div class="col-md-4 pl-1">
+                      <div class="form-group">
+                        <label>Tag</label>
+                        <input type="" class="form-control" placeholder="#" />
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-4 pr-1">
+                      <div class="form-group">
+                        <label>Ngày tạo </label>
+                        <input type="date" class="form-control" placeholder="meta"  name="createdDate" value={values.createdDate} onChange={handleInputChange}/>
+                      </div>
+                    </div>
+                    <div class="col-md-4 px-1">
+                      <div class="form-group">
+                        <label>Ngày sửa</label>
+                        <input type="date"  class="form-control" placeholder="" value="" name="modifiedDate" value={values.modifiedDate} onChange={handleInputChange}/>
+                      </div>
+                    </div>
+                    <div class="col-md-4 pl-1">
+                      <div class="form-group">
+                        <label>Trạng Thái </label>
+                        <input type="" class="form-control" placeholder="true" name="status" value={values.status} onChange={handleInputChange}/>
+                        
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-12">
+                      <div class="form-group">
+                        <label>Mô tả</label>
+                        <textarea class="form-control textarea" name="description" value={values.description} onChange={handleInputChange}>Mô tả cho sản phẩm của bạn </textarea>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="update ml-auto mr-auto">
+                      <button type="submit" class="btn btn-primary btn-round">ADD/UPDATE</button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+        </>
+    )
 }
-
-// export default TaskForm;
